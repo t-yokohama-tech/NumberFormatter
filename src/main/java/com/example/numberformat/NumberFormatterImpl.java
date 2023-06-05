348f8ba14aaed8a064f02c1dcb167a347d8728af
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-06-05 11:29:31 +0900

    リモートへの初期プッシュ

--- src/main/java/com/example/numberformat/NumberFormatterImpl.java
+++ src/main/java/com/example/numberformat/NumberFormatterImpl.java
@@ -12,8 +12,8 @@ public class NumberFormatterImpl implements NumberFormatter {
     public String format(int n) {
 
         // nの範囲指定
-        if(n >= 1 && n <= Integer.MAX_VALUE){
-            // 文字列変換
+        if (n >= 1 && n <= Integer.MAX_VALUE) {
+            // 文字列変換　　
             String str = String.valueOf(n);
             return str;
         }
