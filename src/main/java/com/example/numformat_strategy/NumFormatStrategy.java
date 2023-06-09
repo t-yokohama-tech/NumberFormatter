4aad4550bf2ae0209bbbd7abf97d59ab6c3586df
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-06-09 18:39:49 +0900

    Contextの追加

--- src/main/java/com/example/numformat_strategy/NumFormatStrategy.java
+++ src/main/java/com/example/numformat_strategy/NumFormatStrategy.java
@@ -1,5 +1,13 @@
 package com.example.numformat_strategy;
 
+class NumFormatContext {
+    NumFormatStrategy numFormatStrategy;
+
+    public NumFormatContext(NumFormatStrategy numFormatStrategy) {
+        this.numFormatStrategy = numFormatStrategy;
+    }
+}
+
 public interface NumFormatStrategy {
     String format(int n);
 
