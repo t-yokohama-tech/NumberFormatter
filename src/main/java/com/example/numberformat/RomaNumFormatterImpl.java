91a1c44013afc66d4b97a7c106b97a33a7921d67
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-06-09 11:16:20 +0900

    RomaNumFormaterImpl : ローマ数字変換クラスの追加

---
+++ src/main/java/com/example/numberformat/RomaNumFormatterImpl.java
@@ -0,0 +1,62 @@
+package com.example.numberformat;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+public class RomaNumFormatterImpl extends AbstractNumberFormatter {
+
+    private static final Map<Integer, String> romaOnesPlaceMap = Map.of(
+            1, "Ⅰ",
+            2, "Ⅱ",
+            3, "Ⅲ",
+            4, "Ⅳ",
+            5, "Ⅴ",
+            6, "Ⅵ",
+            7, "Ⅶ",
+            8, "Ⅷ",
+            9, "Ⅸ"
+    );
+    private static final Map<Integer, String> romaTensPlaceMap = Map.of(
+            1, "Ⅹ",
+            2, "ⅩⅩ",
+            3, "ⅩⅩⅩ",
+            4, "ⅩL",
+            5, "L",
+            6, "LⅩ",
+            7, "LXX",
+            8, "LXXX",
+            9, "XC"
+    );
+    private static final Map<Integer, String> romaHundredsPlaceMap = Map.of(
+            1, "C",
+            2, "CC",
+            3, "CCC",
+            4, "CD",
+            5, "D",
+            6, "DC",
+            7, "DCC",
+            8, "DCCC",
+            9, "CM"
+    );
+    private static final Map<Integer, String> romaThousandsPlaceMap = Map.of(
+            1, "M",
+            2, "MM",
+            3, "MMM"
+    );
+    private static final Map<Integer, Map<Integer, String>> romaAllNumMap = Map.of(
+            0, romaOnesPlaceMap,
+            1, romaTensPlaceMap,
+            2, romaHundredsPlaceMap,
+            3, romaThousandsPlaceMap
+    );
+
+    @Override
+    protected String formatDigit(int digit, int place) {
+        return Optional.ofNullable((romaAllNumMap.get(place)).get(digit))
+                .orElse("");
+    }
+
+}
---
+++ src/test/java/com/example/numberformat/RomaNumFormatterImplTest.java
@@ -0,0 +1,27 @@
+package com.example.numberformat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomaNumFormatterImplTest {
+
+    private final RomaNumFormatterImpl romaNumFormatter = new RomaNumFormatterImpl();
+
+
+    @Test
+    public void minRomaTest() {
+        assertEquals("Ⅰ", romaNumFormatter.format(1));
+    }
+
+    @Test
+    public void maxRomaTest() {
+        assertEquals("MMMCMXCⅨ", romaNumFormatter.format(3999));
+    }
+
+    @Test
+    public void WithZeroPtnTest() {
+        assertEquals("MMⅠ", romaNumFormatter.format(2001));
+    }
+
+}
