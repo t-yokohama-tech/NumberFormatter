fb9fd404a39183a4103a5a1fed078ddd9e811305
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-06-13 11:28:04 +0900

    ContextTest：テストメソッド修正

--- src/test/java/com/example/numformat_strategy/NumFormatContextTest.java
+++ src/test/java/com/example/numformat_strategy/NumFormatContextTest.java
@@ -3,72 +3,42 @@ package com.example.numformat_strategy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 public class NumFormatContextTest {
-    private final NumFormatStrategyImpl numFormatStrategy = new NumFormatStrategyImpl();
-
-    private final DaijiNumFormatterImpl daijiNumFormatter = new DaijiNumFormatterImpl();
-
-    private final RomaNumFormatterImpl romaNumFormatter = new RomaNumFormatterImpl();
+    private final NumFormatStrategyImpl numFormatStrategy = mock(NumFormatStrategyImpl.class);
 
     private final NumFormatContext numFormatContext = new NumFormatContext(numFormatStrategy);
 
-    private final NumFormatContext daijiNumFormatContext = new NumFormatContext(daijiNumFormatter);
 
-    private final NumFormatContext romaNumFormatContext = new NumFormatContext(romaNumFormatter);
-
-    // アラビア数字テスト
-    // 最小値「１」パターン
+    // NumFormatContext.format() が適切に strategy.formatDigit() を呼ぶこと
+    // 数値「1」の場合、formatDigitが１度呼ばれることを確認
     @Test
-    public void minNumTest() {
-        assertEquals("1", numFormatContext.format(1));
+    public void callConfirmTest() {
+        int n = 1;
+        when(numFormatStrategy.formatDigit(anyInt(), anyInt())).thenReturn("1");
+        when(numFormatContext.format(n)).thenReturn("1");
+        var result = numFormatContext.format(n);
+
+        // 結果の確認
+        assertEquals("1", result);
+        verify(numFormatStrategy, times(1)).formatDigit(anyInt(), anyInt());
+
     }
 
-    // 最大値「2147483647」パターン
+    // NumFormatContext.format() の戻り値が strategy.formatDigit() の戻り値を結合したもの
     @Test
-    public void maxNumTest() {
-        assertEquals("2147483647", numFormatContext.format(2147483647));
+    public void StrConcatTest() {
+        int n = 123;
+        when(numFormatStrategy.formatDigit(eq(1), anyInt())).thenReturn("a");
+        when(numFormatStrategy.formatDigit(eq(2), anyInt())).thenReturn("b");
+        when(numFormatStrategy.formatDigit(eq(3), anyInt())).thenReturn("c");
+        var result = numFormatContext.format(n);
+
+        // 結果の確認
+        assertEquals("abc", result);
+
     }
-
-
-    //大字数字テスト
-    // 最小値「１」パターン
-    @Test
-    public void minDaijiTest() {
-        assertEquals("壱", daijiNumFormatContext.format(1));
-    }
-
-    // 最大値「99999」パターン
-    @Test
-    public void maxDaijiTest() {
-        assertEquals("玖萬玖仟玖佰玖拾玖", daijiNumFormatContext.format(99999));
-    }
-
-    // ゼロが入るパターン = 文字の出力なし
-    @Test
-    public void withZeroPtn() {
-        assertEquals("壱萬弐佰参", daijiNumFormatContext.format(10203));
-    }
-
-
-    //ローマ数字テスト
-    // 最小値「１」パターン
-    @Test
-    public void minRomaTest() {
-        assertEquals("Ⅰ", romaNumFormatContext.format(1));
-    }
-
-    // 最大値「3999」パターン
-    @Test
-    public void maxRomaTest() {
-        assertEquals("MMMCMXCⅨ", romaNumFormatContext.format(3999));
-    }
-
-    // ゼロが入るパターン = 文字の出力なし
-    @Test
-    public void WithZeroPtnTest() {
-        assertEquals("MMⅠ", romaNumFormatContext.format(2001));
-    }
-
-
 }
