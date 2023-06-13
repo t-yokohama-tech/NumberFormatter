dac6ed0399b720711a03f211e53e87c6588e31e0
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-06-13 17:13:12 +0900

    for文の排除

--- src/test/java/com/example/numformat_strategy/NumFormatContextTest.java
+++ src/test/java/com/example/numformat_strategy/NumFormatContextTest.java
@@ -17,15 +17,21 @@ public class NumFormatContextTest {
     public void callConfirmTest() {
         int n = 12345;
 
-        for (int i = 1; i <= 5; i++) {
-            when(numFormatStrategy.formatDigit(i, 5 - i)).thenReturn(Integer.toString(i));
-        }
+        doReturn(Integer.toString(1)).when(numFormatStrategy).formatDigit(1, 4);
+        doReturn(Integer.toString(2)).when(numFormatStrategy).formatDigit(2, 3);
+        doReturn(Integer.toString(3)).when(numFormatStrategy).formatDigit(3, 2);
+        doReturn(Integer.toString(4)).when(numFormatStrategy).formatDigit(4, 1);
+        doReturn(Integer.toString(5)).when(numFormatStrategy).formatDigit(5, 0);
+
         var result = numFormatContext.format(n);
         // 結果の確認
         assertEquals("12345", result);
-        for (int i = 1; i <= 5; i++) {
-            verify(numFormatStrategy, times(1)).formatDigit(i, 5 - i);
-        }
+
+        verify(numFormatStrategy, times(1)).formatDigit(1, 4);
+        verify(numFormatStrategy, times(1)).formatDigit(2, 3);
+        verify(numFormatStrategy, times(1)).formatDigit(3, 2);
+        verify(numFormatStrategy, times(1)).formatDigit(4, 1);
+        verify(numFormatStrategy, times(1)).formatDigit(5, 0);
 
     }
 
