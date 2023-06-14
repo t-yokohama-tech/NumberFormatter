2b9c54cb829b30f301db5943c4f5d961d199ce45
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-06-14 11:57:43 +0900

    Template Methodパターンで作成したクラスの追加

--- src/main/java/com/example/numformat_strategy/DaijiNumFormatterImpl.java
+++ src/main/java/com/example/numformat_strategy/DaijiNumFormatterImpl.java
@@ -4,51 +4,16 @@ package com.example.numformat_strategy;
 import com.example.numberformat.NumberFormatter;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 
 @Component
 public class DaijiNumFormatterImpl implements NumberFormatter {
 
+    private final DaijiFormatStrategyImpl daijiFormatStrategy = new DaijiFormatStrategyImpl();
+    private final NumFormatContext numFormatContext = new NumFormatContext(daijiFormatStrategy);
+
     @Override
     public String format(int n) {
-        return null;
+        return numFormatContext.format(n);
     }
-
-    private interface DigitFormatter{
-        String format(int place );
-    }
-
-    private static DigitFormatter empty(){
-
-        return place -> "";
-    }
-
-    private static DigitFormatter digit(String digit){
-        return place -> digit + daijiPlaceList.get(place);
-    }
-
-    private static final List<DigitFormatter> digitFormatters = List.of(
-            empty(),     // 0
-            digit("壱"), // 1
-            digit("弐"), // 2
-            digit("参"), // 3
-            digit("肆"), // 4
-            digit("伍"), // 5
-            digit("陸"), // 6
-            digit("漆"), // 7
-            digit("捌"), // 8
-            digit("玖")  // 9
-    );
-    private static final List<String> daijiPlaceList = List.of(
-            "", "拾", "佰", "仟", "萬"
-    );
-
-
-    protected String formatDigit(int digit, int place) {
-        return digitFormatters.get(digit).format(place);
-    }
-
 }
-
 
--- src/main/java/com/example/numformat_strategy/NumberFormatterImpl.java
+++ src/main/java/com/example/numformat_strategy/NumberFormatterImpl.java
@@ -4,24 +4,14 @@ package com.example.numformat_strategy;
 import com.example.numberformat.NumberFormatter;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-
 @Component
 public class NumberFormatterImpl implements NumberFormatter {
 
-    private static final List<String> numberList = List.of(
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-    );
-
-
-    protected String formatDigit(int digit, int place) {
-        // 文字列化処理したものを返却　　
-        return numberList.get(digit);
-    }
+    private final NumFormatStrategy numFormatStrategy = new NumFormatStrategyImpl();
+    private final NumFormatContext numFormatContext = new NumFormatContext(numFormatStrategy);
 
     @Override
     public String format(int n) {
-        return null;
+        return numFormatContext.format(n);
     }
 }
